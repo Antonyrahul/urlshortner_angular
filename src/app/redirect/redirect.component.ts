3a1a01b1c5db106a25634a66a43a90bb2f4d9de3
Antonyrahul <antonyrahul96@gmail.com>
2020-04-24 20:40:20 +0530

    test

--- src/app/redirect/redirect.component.ts
+++ src/app/redirect/redirect.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute,Router,ParamMap} from '@angular/router'
+import {ActivatedRoute} from '@angular/router'
 import { UrlService } from '../url.service'
 
 @Component({
